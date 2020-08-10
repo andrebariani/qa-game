6fb80e5c9bdf43aaea0a7f67bcfcde275578823b
André Matheus Bariani Trava <andre.trava@opus-software.com.br>
2020-08-10 13:58:43 -0300

    no lobby

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import { Lobby } from 'boardgame.io/react';
-import { TicTacToeBoard } from './Board';
-import { TicTacToe } from './Game';
+import React from "react";
+import { render } from "react-dom";
+import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
+import { TicTacToe } from "./Game";
+import { TicTacToeBoard } from "./Board";
 
-const server = `https://${window.location.hostname}`;
-const importedGames = [{
+import css from './index.css';
+import FormClass from "./FormClass";
+import FormFunc from "./FormFunc";
+
+
+const TicTacToeClient = Client({
   game: TicTacToe,
-  board: TicTacToeBoard
-}];
+  board: TicTacToeBoard,
+  numPlayers: 3,
+  multiplayer: SocketIO({ server: "localhost:8000" })
+});
 
-export default () => (
-  <div>
-    <h1>Lobby</h1>
-    <Lobby gameServer={server} lobbyServer={server} gameComponents={importedGames} />
-  </div>
-);
+class App extends React.Component {
+  state = { playerID: null };
+
+  render() {
+    if (this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+          <button onClick={() => this.setState({ playerID: "2" })}>
+            Player 2
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <TicTacToeClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
